2676ad85a86c0e9c15dc1529150b02e1743ede20
Guqo <ryto04@seznam.cz>
2022-04-21 18:59:52 +0200

    03 - Update

--- server.js
+++ server.js
@@ -13,30 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 /* Nastavuje adresu tzv. statických souborů */
 app.use(express.static('public'));
 
-/* Nastavení endpointu, na kterém server provede odpověď a pošle klientovi */
-// app.get('/about', (req, res) => {
-//     res.send('Webserver IT2')
-//     res.end();
-// });
-
-/* Endpoint serveru očekávající dva parametry - user a password */
-/* Příklad URL: http://localhost:3000/secret?user=Pepa&password=007 */
-// app.get('/secret', (req, res) => {
-//     let user = req.query.user;
-//     let password = req.query.password;
-//     res.send(`Uživatel: ${user} | heslo: ${password}`);
-//     res.end();
-// });
-
-// app.get('/save', (req, res) => {
-//     let cvik = req.query.cvik;
-//     let vaha = req.query.vaha;
-//     let serie = req.body.serie;
-//     let opakovani = req.body.opakovani;
-//     res.send(`Cvik: ${cvik} | Váha: ${vaha} | Série: ${serie} | Opakování ${opakovani}`);
-//     res.end();
-// });
-
 const urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
@@ -46,7 +22,6 @@ app.post('/save', urlencodedParser, (req, res) => {
     let vaha = req.body.vaha;
     let serie = req.body.serie;
     let opakovani = req.body.opakovani;
-    let date = new Date();
     let str = `${cvik},${vaha},${serie},${opakovani}\n`;
     fs.appendFile('./data/result.csv', str, function(err) {
         if (err) {
@@ -64,10 +39,9 @@ app.get('/results', (req, res) => {
     csv().fromFile('./data/result.csv')
         .then(data => {
             console.log(data);
-            // res.send(data);
             res.render('results.pug', {
-                'players': data,
-                'nadpis': 'Cviky'
+                cviky: data,
+                nadpis: 'Cviky'
             });
         })
         .catch(err => {
